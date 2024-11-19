efb97bf52258fb6e9cce480c6033452883800284
aimenbashir <aimenbashir999@gmail.com>
2024-11-20 01:40:10 +0500

    Add files via upload

---
+++ assig3.py
@@ -0,0 +1,14 @@
+#Assignment 03
+#tup = (1,2,3,4,5)
+#this is tuple and add string to the start middle and end of this tuple and show the result
+
+#tup=(1,2,3,4,5)
+#s=10
+#m="m"
+#e="e"
+#y= list(tup)
+#y.insert(0,s)
+#y.append(s)
+#i=len(y)//2
+#y.insert(i,s)
+#print(y)
---
+++ assig4.py
@@ -0,0 +1,7 @@
+#Assignment 04
+#Get value from the user of square and find out its area and show the answer
+
+#n= input("enter your number: ")
+#n= float(n)
+#m= n*n
+#print(m)
